88c72e5094ab5eeb6f620379cc43ac20144c590c
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-01 20:55:36 -0700

    Initial Commit

---
+++ VoiceMail/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VoiceMail
+//
+//  Created by Cameron Maiden on 4/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
